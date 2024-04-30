ba80e69ab05aa525ee970b94e3135aa6efa733e6
Meezan Shaikh <meezan@tradexa.co.in>
2024-04-30 08:19:46 +0530

    binary search with list implemented

--- basics/functions.py
+++ basics/functions.py
@@ -1,2 +1,24 @@
-tuple_example = (1,2,3,4,5)
-print(tuple_example[1:3])
+#BINARY SEARCH!!
+a_List = [0,3,4,55,44,67,23,43,5,65,32,12,17,75,18,28,9,2]
+def binary_search(search_list,number_to_search):
+    search_list.sort()
+    high = len(search_list)-1
+    low = 0
+    mid = int((high+low)/2)
+    print(search_list)
+    if(mid < 1 and number_to_search!=search_list[0]):
+        print("number does not exist in the List")
+    if(search_list[mid] == number_to_search):
+        print("Search number found at found", mid)
+    while(search_list[mid]!=number_to_search and mid!=0):
+        print(mid)
+        if(number_to_search > search_list[mid]):
+            low = mid
+            mid = int((high+low)/2)
+        else:
+            high = mid
+            mid = int((high+low)/2)
+    print("Search number found at", mid)
+
+number_to_search = input('Enter a number to search: ')
+binary_search(a_List, int(number_to_search))
